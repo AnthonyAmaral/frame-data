9bf2a83f4cd924f686ec7430f73218b4c8ecbecd
Anthony Amaral <anthonyjohnamaral@gmail.com>
2017-02-08 21:01:55 -0500

    Updated description component with credits for data as well as the project description

--- src/components/description/Description.js
+++ src/components/description/Description.js
@@ -5,12 +5,24 @@ const Example = (props) => {
   return (
     <div>
       <Jumbotron>
-        <h1 className="display-3">Welcome!</h1>
+        <h1 className="display-5">Welcome!</h1>
         <p className="lead">Melee Frames is an online collection of frame data for each character in Melee.</p>
         <hr className="my-2" />
         <p>To begin, click on a character from the side navigation.</p>
-        <p className="lead">
-          <Button color="primary">Learn More</Button>
+        <br/><br/>
+        <h4>About</h4>
+        <p>
+          This project was made as an exercise for me to get more experience learning React.js.
+          I wanted to compile all of the available frame data for each character in Super Smash Bros. Melee into one clean interface.
+          Over time, I will be adding new features to expand on this project. Enjoy!
+        </p>
+        <p className="bold">
+          Special thanks to Stratocasters Hitbox System on SmashBoards for providing the necessary data for this project.
+        </p>
+        <p>
+          <a href="https://smashboards.com/threads/stratocasters-hitbox-system-new-download-link.283973/" target="_blank">
+            <Button color="primary">View SmashBoards Post</Button>
+          </a>
         </p>
       </Jumbotron>
     </div>
